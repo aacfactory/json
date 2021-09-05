120e4f42e5d7b6d7c321d3b569f59cb63284bc73
RyougiNevermore <ryougi.nevermore@hotmail.com>
2021-09-06 05:03:04 +0800

    mod date

--- date.go
+++ date.go
@@ -7,28 +7,18 @@ import (
 )
 
 func NewDate(year int, month time.Month, day int) Date {
-	return Date{
-		Year:  year,
-		Month: month,
-		Day:   day,
-	}
+	t := time.Date(year, month, day, 0, 0, 0, 0, time.Local)
+	return Date(t)
 }
 
-type Date struct {
-	Year  int
-	Month time.Month
-	Day   int
-}
+type Date time.Time
 
 func (d Date) ToTime() time.Time {
-	return time.Date(d.Day, d.Month, d.Day, 0, 0, 0, 0, time.Local)
+	return time.Time(d)
 }
 
 func (d Date) String() string {
-	if d.Month < 10 {
-		return fmt.Sprintf("%d-0%d-%d", d.Year, d.Month, d.Day)
-	}
-	return fmt.Sprintf("%d-%d-%d", d.Year, d.Month, d.Day)
+	return d.ToTime().Format("2006-01-02")
 }
 
 func (d Date) MarshalJSON() (p []byte, err error) {
@@ -48,8 +38,6 @@ func (d *Date) UnmarshalJSON(data []byte) error {
 	if parseErr != nil {
 		return fmt.Errorf("parse date failed for not int, raw value is %s", string(data))
 	}
-	d.Year = date.Year()
-	d.Month = date.Month()
-	d.Day = date.Day()
+	*d = Date(date)
 	return nil
 }
